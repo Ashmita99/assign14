88ee78d676ba61f3a4fc28d7b0a0d28639c0f6e0
Ashmita99 <40290420+Ashmita99@users.noreply.github.com>
2018-07-03 22:56:14 +0530

    Add files via upload

---
+++ classmodule.py
@@ -0,0 +1,42 @@
+#question1
+class Shape:
+    def__ini__(self,length,breadth):
+        self.length=length
+        self.breadth= breadth
+    def area(self):
+            print("The area is ",self.lenghh*self.breadh,"units")
+        class Rectangle(Shape):
+            def__init__(self):
+            super(Rectangle,self).__init__()
+            class Square(shape):
+                def__init__(self):
+                    Super(square,self).__init__()
+                rec=Shappe(2,4)
+            sq=shape(4,4)
+            sq.area()
+			
+#question3
+class Cop:
+ def__init__(self,name,exprience):
+  self.name=name
+  self.experience=experience
+ def display_details(self):
+  print(self.name,"cop is on duty with", self exprience,"year of exprience")
+  def update_detail(self):
+   self.name='Sherlock'
+   self.experiencce=7
+   class Mission(Cop):
+    def__init(self):
+    super(Mission,self).__init()
+    def dislay_mission(self,mission):
+     print("the active mission ssign",mission)
+
+     def assign_cop(self,mission):
+      print(self.name,"has been assigned ",mission)
+      mission.disply_mission('Secret'
+      mission.display_details()
+      mission.assign_cop("Secret")
+
+	
+		
+		
